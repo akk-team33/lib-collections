afdcbc78b77df7d8bab9ac5c42f4b6df81cee1a7
Andi Kluge <andi.kluge@mobi4friends.de>
2017-03-13 17:47:04 +0100

    Neustart

--- src/test/java/net/team33/collections/Collection.java
+++ src/test/java/net/team33/collections/Collection.java
@@ -1,82 +1,101 @@
 package net.team33.collections;
 
-import java.util.Iterator;
-import java.util.Spliterator;
 import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.function.IntFunction;
 import java.util.stream.Stream;
 
-public class Collection<E> implements java.util.Collection<E> {
+public interface Collection<E> {
 
-    @Override public int size() {
-        throw new UnsupportedOperationException("not yet implemented");
+    /**
+     * @return The size of the Collection, the number of contained elements.
+     */
+    int size();
+
+    /**
+     * Determines whether the Collection is empty.
+     */
+    default boolean isEmpty() {
+        return 0 == size();
     }
 
-    @Override public boolean isEmpty() {
-        throw new UnsupportedOperationException("not yet implemented");
+    /**
+     * Determines whether the Collection contains a specific element.
+     */
+    boolean contains(final Object o);
+
+    /**
+     * Determines whether the Collection contains all of some specific elements.
+     */
+    default boolean containsAll(final Collection<?> other) {
+        return containsAll(other.stream());
     }
 
-    @Override public boolean contains(final Object o) {
-        throw new UnsupportedOperationException("not yet implemented");
+    /**
+     * Determines whether the Collection contains all of some specific elements.
+     */
+    default boolean containsAll(final Stream<?> other) {
+        return other.allMatch(this::contains);
     }
 
-    @Override public Iterator<E> iterator() {
-        throw new UnsupportedOperationException("not yet implemented");
+    default void forEach(final Consumer<? super E> action) {
+        stream().forEach(action);
     }
 
-    @Override public void forEach(final Consumer<? super E> action) {
-        throw new UnsupportedOperationException("not yet implemented");
+    default Object[] toArray() {
+        return stream().toArray();
     }
 
-    @Override public Object[] toArray() {
-        throw new UnsupportedOperationException("not yet implemented");
+    default <T> T[] toArray(final IntFunction<T[]> generator) {
+        return stream().toArray(generator);
     }
 
-    @Override public <T> T[] toArray(final T[] a) {
-        throw new UnsupportedOperationException("not yet implemented");
+    /**
+     * @return The contained elements as a Stream.
+     */
+    Stream<E> stream();
+
+    /**
+     * @return The contained elements as a Stream. Probably a parallel Stream.
+     */
+    Stream<E> parallelStream();
+
+    interface Builder<E, C extends Collection<E>, B extends Builder<E, C, B>> {
+
+        default B add(final E... elements) {
+            return addAll(Stream.of(elements));
+        }
+
+        default B addAll(final Collection<? extends E> elements) {
+            return addAll(elements.stream());
+        }
+
+        B addAll(Stream<? extends E> elements);
+
+        default B remove(final Object... elements) {
+            return removeAll(Stream.of(elements));
+        }
+
+        default B removeAll(final Collection<?> elements) {
+            return removeAll(elements.stream());
+        }
+
+        B removeAll(Stream<?> elements);
+
+        default B retain(final Object... elements) {
+            return retainAll(Stream.of(elements));
+        }
+
+        default B retainAll(final Collection<?> elements) {
+            return retainAll(elements.stream());
+        }
+
+        B retainAll(Stream<?> elements);
+
+        B clear();
+
+        C build();
     }
 
-    @Override public boolean add(final E e) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public boolean remove(final Object o) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public boolean containsAll(final java.util.Collection<?> c) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public boolean addAll(final java.util.Collection<? extends E> c) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public boolean removeAll(final java.util.Collection<?> c) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public boolean removeIf(final Predicate<? super E> filter) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public boolean retainAll(final java.util.Collection<?> c) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public void clear() {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public Spliterator<E> spliterator() {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public Stream<E> stream() {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override public Stream<E> parallelStream() {
-        throw new UnsupportedOperationException("not yet implemented");
+    abstract class Base<E> implements Collection<E> {
     }
 }
