076b63e760989139e829c080a1dc5f4c51218fa4
andi <akk-pg@team33.de>
2020-10-28 09:51:57 +0100

    Initial implementation and test

--- src/main/java/de/team33/libs/collections/v1/Collector.java
+++ src/main/java/de/team33/libs/collections/v1/Collector.java
@@ -1,31 +1,88 @@
 package de.team33.libs.collections.v1;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+import static java.util.Collections.singleton;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A tool class that is used to manipulate collections in a builder style.
+ *
+ * @param <E> The supported element type
+ * @param <C> The supported type of {@link Collection}
+ */
 public class Collector<E, C extends Collection<E>> {
 
+    private final C subject;
 
+    public Collector(final C subject) throws NullPointerException {
+        this.subject = requireNonNull(subject);
+    }
+
+    public static <E, C extends Collection<E>> Collector<E, C> on(final C subject) throws NullPointerException {
+        return new Collector<>(subject);
+    }
 
     /**
      * @see Collection#add(Object)
      */
-    public final Collector<E, C> add(final E e) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    /**
-     * @see Collection#remove(Object)
-     */
-    public final Collector<E, C> remove(final Object o) {
-        throw new UnsupportedOperationException("not yet implemented");
+    public final Collector<E, C> add(final E element) {
+        subject.add(element);
+        return this;
     }
 
     /**
      * @see Collection#addAll(Collection)
      */
-    public final Collector<E, C> addAll(final Collection<? extends E> c) {
-        throw new UnsupportedOperationException("not yet implemented");
+    @SafeVarargs
+    public final Collector<E, C> add(final E ... elements) {
+        return addAll(Arrays.asList(elements));
+    }
+
+    /**
+     * @see Collection#addAll(Collection)
+     */
+    public final Collector<E, C> addAll(final Collection<? extends E> elements) {
+        subject.addAll(elements);
+        return this;
+    }
+
+    /**
+     * @see Collection#addAll(Collection)
+     */
+    public final Collector<E, C> addAll(final Stream<? extends E> elements) {
+        elements.forEach(subject::add);
+        return this;
+    }
+
+    /**
+     * @see Collection#addAll(Collection)
+     */
+    public final Collector<E, C> addAll(final Iterable<? extends E> elements) {
+        if (elements instanceof Collection) {
+            // addAll(Collection) may be more efficient than add(E) for each element
+            // noinspection unchecked
+            return addAll((Collection<? extends E>)elements);
+        }
+        elements.forEach(subject::add);
+        return this;
+    }
+
+    /**
+     * @see Collection#remove(Object)
+     */
+    public final Collector<E, C> remove(final Object element) {
+        try {
+            subject.removeAll(singleton(element));
+        } catch (final NullPointerException | ClassCastException ignored) {
+            // --> <subject> can not contain <element>
+            // --> <subject> simply does not contain <element>
+            // --> Nothing to do!
+        }
+        return this;
     }
 
     /**
@@ -54,5 +111,9 @@ public class Collector<E, C extends Collection<E>> {
      */
     public final Collector<E, C> clear() {
         throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    public final C getSubject() {
+        return subject;
     }
 }
--- src/test/java/de/team33/test/collections/v1/CollectorTest.java
+++ src/test/java/de/team33/test/collections/v1/CollectorTest.java
@@ -1,7 +1,145 @@
 package de.team33.test.collections.v1;
 
-import static org.junit.Assert.*;
+import de.team33.libs.collections.v1.Collector;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class CollectorTest {
 
+    private final Random random = new Random();
+    private final List<Integer> template;
+
+    public CollectorTest() {
+        final int size = 8 + random.nextInt(8);
+        this.template = new ArrayList<>(size);
+        Stream.generate(random::nextInt)
+              .limit(size)
+              .forEach(template::add);
+    }
+
+    /**
+     * Ensures that {@link Collector#getSubject()} returns the {@link Collector#on(Collection) originally associated}
+     * instance.
+     */
+    @Test
+    public final void getSubject() {
+        // Preconditions ...
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        final Collection<Integer> initial = new HashSet<>(template);
+        final Collection<Integer> original = new HashSet<>(initial);
+        // the original must be equivalent to the initial value ...
+        assertEquals(initial, original);
+        // ... but of course not identical ...
+        assertNotSame(initial, original);
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        // Even if the associated instance has been changed, ...
+        final Collection<Integer> result = Collector.on(original)
+                                                    .add(random.nextInt())
+                                                    .remove(template.get(5))
+                                                    .getSubject();
+        // ... the result must be identical to the original ...
+        assertSame(original, result);
+        // ... but of course not equivalent to the initial value.
+        assertNotEquals(initial, result);
+    }
+
+    /**
+     * Ensures that {@link Collector#add(Object)} has the same effect as {@link Collection#add(Object)}.
+     */
+    @Test
+    public final void add() {
+        final LinkedList<Object> result = Collector.on(new LinkedList<>())
+                                                   .add(template.get(0))
+                                                   .add(template.get(1))
+                                                   .add(template.get(2))
+                                                   .add(template.get(3))
+                                                   .add(template.get(4))
+                                                   .getSubject();
+        assertEquals(template.subList(0, 5), result);
+    }
+
+    /**
+     * Ensures that {@link Collector#remove(Object) remove(null)} does not result in a {@link NullPointerException},
+     * even if the {@link Collector#on(Collection) originally associated} instance cannot contain {@code null} elements.
+     */
+    @Test
+    public final void removeNull() {
+        final Collection<Integer> initial = new HashSet<>(asList(1, 2, 3));
+        final Collection<Integer> subject = new TreeSet<>(initial);
+        try {
+            subject.remove(null);
+            fail("subject.remove(null) should result in a NullPointerException");
+        } catch (final NullPointerException ignored) {
+            // is expected;
+        }
+
+        final Collection<Integer> result = Collector.on(subject)
+                                                    .remove(null)
+                                                    .getSubject();
+        // should work - but do nothing ...
+        assertEquals(initial, result);
+    }
+
+    /**
+     * Ensures that {@link Collector#remove(Object) remove(any)} does not result in a {@link ClassCastException},
+     * even if the {@link Collector#on(Collection) originally associated} instance cannot contain {@code any} element.
+     */
+    @Test
+    public final void removeAny() {
+        final Collection<Integer> initial = new HashSet<>(asList(1, 2, 3));
+        final Collection<Integer> subject = new TreeSet<>(initial);
+        try {
+            subject.remove("a string");
+            fail("subject.remove(\"a string\") should result in a ClassCastException");
+        } catch (final ClassCastException ignored) {
+            // is expected;
+        }
+
+        final Collection<Integer> result = Collector.on(subject)
+                                                    .remove("a string")
+                                                    .getSubject();
+        // should work - but do nothing ...
+        assertEquals(initial, result);
+    }
+
+    @Test
+    public final void remove() {
+        final Collection<Integer> subject = new ArrayList<>(asList(1, 1, 1, 1, 1, 1, 1));
+        assertEquals(7, subject.size());
+        assertTrue(subject.contains(1));
+
+        // Should remove only one (the first?) element of value <1> ...
+        subject.remove(1);
+        assertEquals(6, subject.size());
+        assertTrue(subject.contains(1));
+
+        // Must remove all remaining elements of value <1> ...
+        Collector.on(subject)
+                 .remove(1);
+        assertEquals(0, subject.size());
+        assertFalse(subject.contains(1));
+    }
+
+    @Test
+    public final void test() {
+        fail("not yet implemented");
+    }
 }
